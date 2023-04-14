0ace3a417f7b369a3dc89107459240a14ff827c3
Ashish <ashishjayram765@gmail.com>
2023-04-14 12:28:09 +0530

    wer

--- Project/front-end/src/views/profile/Profile.js
+++ Project/front-end/src/views/profile/Profile.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import styles from './Profile.module.css'
-import ppic from '../../static/person.png'
+import ppic from '../../static/author.jpeg'
 import back from '../../static/dashback.jpeg'
 function Profile() {
     document.body.style.backgroundImage = `url(${back})`;
     document.body.style.backgroundSize = "1600px 800px";
     document.body.style.backgroundRepeat = "no-repeat";
     const arr = {
-        username: "Aditya Deshpande",
-        about: "I am keen writer and very much interested in sharing my thoughts,opinions and experience with other.Likewise, the number of sentences in a paragraph can change based on the style of the writer. Some authors prefer longer, more descriptive paragraphs, while other authors prefer shorter, faster-paced paragraphs.Last, the conclusion wraps up the idea, sometimes summarizing what’s been presented or transitioning to the next paragraph. The content of the conclusion depends on the type of paragraph, and it’s often acceptable to end a paragraph with a final piece of support that concludes the thought instead of a summary. ",
-        ppic: require('../../static/person.png'),
+        username: "Austin kleon",
+        about: "Austin Kleon is the New York Times bestselling author of a trilogy of illustrated books about creativity in the digital age: Steal Like An Artist, Show Your Work!, and Keep Going. He’s also the author of Newspaper Blackout, a collection of poems made by redacting the newspaper with a permanent marker. His books have been translated into dozens of languages and have sold over a million copies worldwide. He’s been featured on NPR’s Morning Edition, PBS Newshour, and in The New York Times and The Wall Street Journal. New York Magazine called his work “brilliant,” The Atlantic called him “positively one of the most interesting people on the Internet,” and The New Yorker said his poems “resurrect the newspaper when everybody else is declaring it dead.” He speaks for organizations such as Pixar, Google, SXSW, TEDx, and The Economist. In previous lives, he worked as a librarian, a web designer, and an advertising copywriter. He lives in Austin, Texas, with his wife and sons. ",
+        ppic: require('../../static/author.jpeg'),
         nblogs: "2",
         pviews: "2",
         popularity: "30%"
@@ -27,7 +27,7 @@ function Profile() {
                 <div className={styles.wrdetails}>
 
                     <p> Contact : 927138122</p>
-                    <p> Email : xyz@gmail.com</p>
+                    <p> Email : austin@austinkleon.com</p>
 
                 </div>
 
