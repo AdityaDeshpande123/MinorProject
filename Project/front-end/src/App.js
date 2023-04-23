fb877df0bade19878d4ecf6dd87a08dd0605d764
ashwin4945 <ashwin4945@gmail.com>
2023-04-23 12:13:21 +0530

    ashcommtwelve

--- Project/front-end/src/App.js
+++ Project/front-end/src/App.js
@@ -6,17 +6,12 @@ import Card from './views/card-view/Card';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from './views/profile/Profile';
 import Tempone from './views/blog_create/Tempone';
-<<<<<<< HEAD
-import Template1 from './views/blog_create/template/Template1'
-import Template2 from './views/blog_create/template/Template2'
-
-=======
-import Template1 from './views/blog_create/template/Template1.js'
-import Template2 from './views/blog_create/template/Template2.js'
-import Template3 from './views/blog_create/template/Template3.js'
+import Template1 from './views/blog_create/template/Template1.js';
+import Template2 from './views/blog_create/template/Template2.js';
+import Template3 from './views/blog_create/template/Template3.js';
 import Publogs from './views/profile/publishblogs/Publogs';
 import Savblogs from './views/profile/savedblogs/Savblogs';
->>>>>>> 62bd32ff626bc268fb525e1be12c39602407a86a
+
 function App() {
 
   return (
@@ -24,7 +19,7 @@ function App() {
     <div>
 
 
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage></Homepage>}></Route>
           <Route path='/signup' element={<Signpage></Signpage>}></Route>
@@ -34,26 +29,17 @@ function App() {
           <Route exact path='/createblog/temp2' element={<Template2></Template2>}></Route>
           <Route exact path='/createblog/temp3' element={<Template3></Template3>}></Route>
           <Route exact path='/profile/publishblog' element={<Publogs></Publogs>}></Route>
-          <Route exact path='/profile/"savedblog"' element={<Savblogs></Savblogs>}></Route>
+          <Route exact path='/profile/savedblog' element={<Savblogs></Savblogs>}></Route>
 
         </Routes>
 
 
-<<<<<<< HEAD
-  </BrowserRouter> */}
-
-   {/* <Tempone></Tempone>  */}
-
-   <Template2></Template2>
-  
-=======
 
       </BrowserRouter>
 
       {/* <Tempone></Tempone> */}
 
 
->>>>>>> 62bd32ff626bc268fb525e1be12c39602407a86a
 
 
     </div>
