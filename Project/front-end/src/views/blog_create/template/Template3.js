d19a04cb3f3deec925ba82d5357a54485860570b
Praneeth Hebbar <khpraneeth@gmail.com>
2023-05-28 14:43:00 +0530

    pdfcommit

--- Project/front-end/src/views/blog_create/template/Template3.js
+++ Project/front-end/src/views/blog_create/template/Template3.js
@@ -3,8 +3,31 @@ import React,{useState} from "react";
 import person from '../../../static/person.png';
 import signin from '../../../static/signin.jpg';
 import { useNavigate } from 'react-router-dom';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import {useRef} from 'react';
 
 function Template() {
+  const pdfRef = useRef();
+      
+        const handleConvertToPDF = () => {
+          const input = pdfRef.current;
+      
+          html2canvas(input)
+            .then((canvas) => {
+              const imgData = canvas.toDataURL('image/png');
+              const pdf = new jsPDF();
+              const pdfWidth=pdf.internal.pageSize.getWidth();
+            const pdfHeight=pdf.internal.pageSize.getHeight();
+            const imgWidth=canvas.width;
+            const imgHeight=canvas.height;
+            const ratio=Math.min(pdfWidth/imgWidth, pdfHeight/imgHeight);
+            const imgX=(pdfWidth-imgWidth*ratio)/2;
+            const imgY=(pdfHeight-imgHeight*ratio)/2;
+            pdf.addImage(imgData,'PNG',imgX,imgY,imgWidth*ratio,imgHeight*ratio);
+              pdf.save('template.pdf');
+            });
+        };
   const [file, setFile] = useState();
   const [file1, setFile1] = useState();
   const [file2, setFile2] = useState();
@@ -32,7 +55,7 @@ function Template() {
           <h1 className={style.base_h1}>BLOGOSPHERE</h1>
       </div>
       <form method="post" target="_self" action="">
-            <div className={style.temp3}>
+            <div className={style.temp3} ref={pdfRef}>
             <div className={style.title}>
             <textarea id="title" name="title" style={{textDecorationThickness:"100%",border: "0px solid black",width:"140vh",backgroundColor:"white"}}></textarea>
             </div>
@@ -86,10 +109,10 @@ function Template() {
             <div className={style.mt}>
             <textarea id="text3" name="text3" style={{width:"84vh",height: "88vh",border: "0px solid black",backgroundColor:"white",padding:"10px"}}></textarea>
             </div>
-            <button type="submit" className={style.b3}> Publish</button>
-            <button type="draft" className={style.b4}>Save as draft</button>
             </div>
             </form>
+            <button type="submit" className={style.b3} onClick={handleConvertToPDF}> Publish</button>
+            <button type="draft" className={style.b4}>Save as draft</button>
             <footer className={style.About}>
             <img className={style.fimg}  style={{width:"21vh",height:"21vh"}}></img>
             <div className={style.info}>
